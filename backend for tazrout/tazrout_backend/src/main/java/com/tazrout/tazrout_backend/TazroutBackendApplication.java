0ed05fc94ccf571103546a82db03dfd15205c65d
M071m2d <fehisadek@gmail.com>
2026-02-15 23:40:03 +0100

    first commit

---
+++ backend for tazrout/tazrout_backend/src/main/java/com/tazrout/tazrout_backend/TazroutBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tazrout.tazrout_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TazroutBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TazroutBackendApplication.class, args);
+	}
+
+}
---
+++ backend for tazrout/tazrout_backend/src/test/java/com/tazrout/tazrout_backend/TazroutBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tazrout.tazrout_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TazroutBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
